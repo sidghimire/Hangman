3bd1c58b9d0b0ed3206ce9fe450c21d3d7c90527
Sid Ghimire <siddharthaghimire@gmail.com>
2021-10-08 21:00:43 +0545

    Update index.py

--- index.py
+++ index.py
@@ -3,7 +3,7 @@ import pygame.freetype
 import string
 import random
 
-words=['CAR','BUS','BABY','LAMBORGHINI']
+words=['CAR','BUS','BABY','LAMBORGHINI','LOVELY','PUPPY','CUTE','HUG','SKY','DOG','CAT','DOUBLE']
 choice=random.choice(words)
 word_list=list(choice)
 collection=[]
@@ -13,6 +13,8 @@ for empty in range(len(word_list)):
 mouseX=None
 mouseY=None
 countOnce=0
+mistake=0
+won=0
 btn=0
 pygame.init()
 size=width,height=800,700
@@ -20,6 +22,28 @@ black=0,0,0
 screen=pygame.display.set_mode(size)
 white=pygame.color.Color('#ffffff')
 alphabet=[""]
+
+
+def draw_hanger(x,y):
+    pygame.draw.rect(screen, black,(x,y,10,200))
+    pygame.draw.rect(screen, black,(x,y,100,10))
+    pygame.draw.rect(screen, black,(x+100,y,10,40))
+
+def make_mistake(x,y):
+    if(mistake >= 1):
+        pygame.draw.circle(screen, black, (x+5,y+25), 30)
+        if(mistake >= 2):
+            pygame.draw.rect(screen, black,(x+5,y+40,10,90))
+            if(mistake >= 3):
+                pygame.draw.line(screen, black, (x+10,y+60), (x-35,y+100),width=10)
+                if(mistake >= 4):
+                    pygame.draw.line(screen, black, (x+10,y+60), (x+45,y+100),width=10)
+                    if(mistake >= 5):
+                        pygame.draw.line(screen, black, (x+10,y+120), (x-35,y+180),width=10)
+                        if(mistake >= 6):
+                            pygame.draw.line(screen, black, (x+10,y+120), (x+45,y+180),width=10)
+                            display_text(black, 40, 340, 350, "Game Over")
+    
 
 def getAlphabet():
     for x in range(ord('A'),ord('Z')+1):
@@ -91,16 +115,16 @@ def make_blank_space():
             text=font.render(ans[x],True,black)
         screen.blit(text,(initX,initY))   
         initX+=50
-        
-        
-        
 getAlphabet()     
 #Main        
 while 1:
-    
     events = pygame.event.get()
     for event in events:
         if event.type == pygame.MOUSEBUTTONDOWN:
+            if("" not in ans):
+                mistake=6
+            if(mistake>=6 or won==1):
+                exit()
             mouseX= pygame.mouse.get_pos()[0]
             mouseY= pygame.mouse.get_pos()[1]
             for i in range(26):
@@ -113,19 +137,19 @@ while 1:
                             ans[j]=word_list[j]
                             count=count+1
                     if count==0:
-                        print("Punish")
+                        mistake+=1;
                     else:
+                        if("" not in ans):
+                            won=1
                         break    
-                    
         if event.type == pygame.QUIT:sys.exit()
-    
- 
-    
-
     make_blank_space()
     display_text(black, 40, 340, 30, "Hangman")
     make_keyboard()
-    
+    make_mistake(400,140)
+    draw_hanger(300,100)
+    if(won==1):
+        display_text(black, 40, 340, 350, "You Win")
     pygame.display.flip()
     screen.fill(white)
 pygame.time.wait(5000)
